69e58b50e69c15117f1403aaea1aca74d32511b0
Bormoley1983 <zhuravel_leonid@yahoo.com>
2026-08-31 08:52:59 +0300

    fix: correct user-service host configuration in application.yaml and update FeignClient URL in UserServiceClient

--- src/main/java/faang/school/achievement/client/UserServiceClient.java
+++ src/main/java/faang/school/achievement/client/UserServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+@FeignClient(name = "user-service", url = "http://${user-service.host}:${user-service.port}")
 public interface UserServiceClient {
     
     //TODO: replacement of foreign key in db beacuse of total separation of domains on microservice layer
