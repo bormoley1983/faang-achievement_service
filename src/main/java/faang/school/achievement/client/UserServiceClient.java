4af631092872e193c64b6bb051901ba7af2e22d7
Bormoley1983 <zhuravel_leonid@yahoo.com>
2026-09-01 19:52:59 +0300

    refactor: remove outdated TODO comment regarding foreign key replacement in UserServiceClient

--- src/main/java/faang/school/achievement/client/UserServiceClient.java
+++ src/main/java/faang/school/achievement/client/UserServiceClient.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "user-service", url = "http://${user-service.host}:${user-service.port}")
 public interface UserServiceClient {
-    
-    //TODO: replacement of foreign key in db beacuse of total separation of domains on microservice layer
+
     @GetMapping("/users/{id}/exists")
     boolean userExists(@PathVariable("id") long userId);
 }
